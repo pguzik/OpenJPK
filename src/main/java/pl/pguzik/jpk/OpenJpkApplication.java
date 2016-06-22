05bd15df5c8f414de50a9300afbfc8bf3cef8ad5
Piotr Guzik <piotr.guzik@allegrogroup.com>
2016-06-22 21:13:56 +0200

    initial commit

---
+++ src/main/java/pl/pguzik/jpk/OpenJpkApplication.java
@@ -0,0 +1,12 @@
+package pl.pguzik.jpk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenJpkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenJpkApplication.class, args);
+	}
+}
