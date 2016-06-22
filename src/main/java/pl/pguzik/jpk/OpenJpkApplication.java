297cfd32a06a7ee2414a7d9994ec8d3f35a00236
Piotr Guzik <piotr.guzik@allegrogroup.com>
2016-06-22 22:03:23 +0200

    added file upload

--- src/main/java/pl/pguzik/jpk/OpenJpkApplication.java
+++ src/main/java/pl/pguzik/jpk/OpenJpkApplication.java
@@ -1,12 +1,25 @@
 package pl.pguzik.jpk;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.File;
 
 @SpringBootApplication
 public class OpenJpkApplication {
 
+	public static String ROOT = "upload-dir";
+
 	public static void main(String[] args) {
 		SpringApplication.run(OpenJpkApplication.class, args);
 	}
+
+    @Bean
+    CommandLineRunner init() {
+        return (String[] args) -> {
+            new File(ROOT).mkdir();
+        };
+    }
 }
